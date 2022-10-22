ade323507cee72d14035528003231e01e8fb54db
leonseet <kiangyeow@hotmail.com>
2022-10-22 15:19:47 +0800

    paused at 3.5

--- part3/phonebooks/index.js
+++ part3/phonebooks/index.js
@@ -30,11 +30,29 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+    response.json(person)
+    } else {
+    response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
     response.send(`<div>
-    <div>Phonebook has info for ${persons.length} people</div>
-    <div>test</div>
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>
     </div>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
 })
 
 const PORT = 3001
