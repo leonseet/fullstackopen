87f5af3b135561838097d0ee3a59c9da00745895
leonseet <kiangyeow@gmail.com>
2022-10-24 09:38:40 +0800

    completed exercise 3.12

--- part3/phonebooks/index.js
+++ part3/phonebooks/index.js
@@ -115,6 +115,12 @@ persons = persons.concat(person)
 response.json(person)
 })
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
