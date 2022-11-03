0c3bbde76363f68b4740fb87a22d61516ab02491
leonseet <kiangyeow@hotmail.com>
2022-11-03 08:59:37 +0800

    paused at 4.16 with mongoose validation error crashing app

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -2,15 +2,26 @@ const config = require("./utils/config")
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const blogRouter = require("./controllers/blog")
 const mongoose = require('mongoose')
+
+const blogRouter = require("./controllers/blog")
+const userRouter = require("./controllers/user")
+const middleware =require("./utils/middleware")
+const logger = require("./utils/logger")
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+    .then(() => logger.info("connected tp MongoDB"))
+    // .catch((error) => logger.error("error connection to MongoDB:", error.message))
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogRouter)
+app.use("/api/users", userRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
